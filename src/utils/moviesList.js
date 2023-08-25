f34b30b805a581a3bcfdf123383a7d6b6f143586
Maxim Perepletchikov <maxwell-38@yandex.ru>
2023-08-25 13:38:26 +0500

    feat: movies

---
+++ src/utils/moviesList.js
@@ -0,0 +1,85 @@
+import movie1 from '../images/movies-list/1.jpg';
+import movie2 from '../images/movies-list/2.jpg';
+import movie3 from '../images/movies-list/3.jpg';
+import movie4 from '../images/movies-list/4.jpg';
+import movie5 from '../images/movies-list/5.jpg';
+import movie6 from '../images/movies-list/6.jpg';
+import movie7 from '../images/movies-list/7.jpg';
+import movie8 from '../images/movies-list/8.jpg';
+import movie9 from '../images/movies-list/9.jpg';
+import movie10 from '../images/movies-list/10.jpg';
+import movie11 from '../images/movies-list/11.jpg';
+import movie12 from '../images/movies-list/12.jpg';
+import movie13 from '../images/movies-list/13.jpg';
+import movie14 from '../images/movies-list/14.jpg';
+import movie15 from '../images/movies-list/15.jpg';
+import movie16 from '../images/movies-list/16.jpg';
+
+const movies = [
+  {
+    id: 1,
+    image: movie1,
+  },
+  {
+    id: 2,
+    image: movie2,
+  },
+  {
+    id: 3,
+    image: movie3,
+  },
+  {
+    id: 4,
+    image: movie4,
+  },
+  {
+    id: 5,
+    image: movie5,
+  },
+  {
+    id: 6,
+    image: movie6,
+  },
+  {
+    id: 7,
+    image: movie7,
+  },
+  {
+    id: 8,
+    image: movie8,
+  },
+  {
+    id: 9,
+    image: movie9,
+  },
+  {
+    id: 10,
+    image: movie10,
+  },
+  {
+    id: 11,
+    image: movie11,
+  },
+  {
+    id: 12,
+    image: movie12,
+  },
+  {
+    id: 13,
+    image: movie13,
+  },
+  {
+    id: 14,
+    image: movie14,
+  },
+  {
+    id: 15,
+    image: movie15,
+  },
+  {
+    id: 16,
+    image: movie16,
+  },
+];
+
+export default movies;
